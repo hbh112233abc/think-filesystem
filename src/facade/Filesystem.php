e460a6db90a67bd8edfa70f1eb80e0d9f104d8fe
hbh112233abc <hbh112233abc@163.com>
2025-12-25 18:26:45 +0800

    getDiskConfig 提示改进

--- src/facade/Filesystem.php
+++ src/facade/Filesystem.php
@@ -1,11 +1,11 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace bingher\filesystem\facade;
 
-use think\Facade;
 use bingher\filesystem\Driver;
+use think\Facade;
 
 /**
  * Class Filesystem
@@ -14,7 +14,7 @@ use bingher\filesystem\Driver;
  * @method static Driver disk( string $name = null ) ,null|string
  * @method static Driver cloud( string $name = null ) ,null|string
  * @method static mixed getConfig( null|string $name = null,mixed $default = null ) 获取缓存配置
- * @method static array getDiskConfig( string $disk,null $name = null,null $default = null ) 获取磁盘配置
+ * @method static array|string getDiskConfig( string $disk,string $name = null,mixed $default = null ) 获取磁盘配置
  * @method static string|null getDefaultDriver() 默认驱动
  */
 class Filesystem extends Facade
